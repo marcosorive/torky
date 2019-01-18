7149b1112805be1a20400c532e9f515467172bcb
Marcos Orive Izarra <marcos@orive.me>
2019-01-18 23:10:54 +0100

    Updates

---
+++ precache-manifest.7324a41c8d7eea59e3a625064931042b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "34b67051d146342d06c7",
+    "url": "/torky/static/js/runtime~main.34b67051.js"
+  },
+  {
+    "revision": "5c4524ac83efa1a0bc11",
+    "url": "/torky/static/js/main.5c4524ac.chunk.js"
+  },
+  {
+    "revision": "3d62dca1c589c0df6bcf",
+    "url": "/torky/static/js/1.3d62dca1.chunk.js"
+  },
+  {
+    "revision": "5c4524ac83efa1a0bc11",
+    "url": "/torky/static/css/main.b431cdca.chunk.css"
+  },
+  {
+    "revision": "af7afeeca2d01d85f2615259f9e9bcf0",
+    "url": "/torky/index.html"
+  }
+];
